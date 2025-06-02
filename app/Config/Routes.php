11e9d99d0a85b79dcdffb29f59ba355b9818744d
ApliCalvin <thecalvins.sims@gmail.com>
2025-06-02 22:51:01 +0700

    [init] First Initialize - File Pertemuan 11

---
+++ app/Config/Routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login', ['filter' => 'redirect']);
+$routes->get('logout', 'AuthController::logout');
+
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+$routes->get('faq', 'Home::faq', ['filter' => 'auth']);
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
---
+++ app/Controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+
+        
+        return view('v_home');
+    }
+
+    public function faq(): string 
+    {
+        return view('v_faq');
+    }
+}
