8e17312442baf2ae6bb629762297c0ba220de3c2
ApliCalvin <thecalvins.sims@gmail.com>
2025-06-09 22:32:56 +0700

    Fix some kesalahan

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'root',
+        'password'     => 'simbolon',
+        'database'     => 'db_ci4',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
